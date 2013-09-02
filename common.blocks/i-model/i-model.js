8fa15d337d309b8609976948fdce2d7d5feb11ee
Alexey Kalinin <alkaline@yandex-team.ru>
2013-09-02 13:15:21 +0400

    buildDeps: колбек сортировки должен возвращать Number

--- common.blocks/i-model/i-model.js
+++ common.blocks/i-model/i-model.js
@@ -567,7 +567,7 @@
             fieldNames.forEach(function(fieldName) {
                 if (deps[fieldName])
                     fieldDecl[fieldName].dependsTo = deps[fieldName].sort(function(a, b) {
-                        return deps[b] && (deps[b].indexOf(a) != -1 ? 1 : -1);
+                        return deps[b] ? (deps[b].indexOf(a) != -1 ? 1 : -1) : 0;
                     });
             });
 
