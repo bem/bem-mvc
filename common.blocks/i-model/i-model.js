0e739f8c7b7c388ed3eb00bc27374c62b9319d02
belyanskii <belyanskii@ya.ru>
2014-05-20 16:32:03 +0400

    i-model: Неправильное построение пути к моделе, если id предка - 0.

--- common.blocks/i-model/i-model.js
+++ common.blocks/i-model/i-model.js
@@ -941,7 +941,7 @@
 
                 parts[el] = model && model.path() ||
                     (typeof path === 'object' ? MODEL.buildPath(path) : path) ||
-                    (name ? name + (id ? ID_SEPARATOR + id : '') : '');
+                    (name ? name + (!isNaN(parseFloat(id)) && isFinite(id) ? ID_SEPARATOR + id : '') : '');
             });
 
             return (parts.parent ? parts.parent + CHILD_SEPARATOR : '') +
