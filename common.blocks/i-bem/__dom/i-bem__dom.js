ab6c2f5ae8743c3a02eeaa3540a23d75ed467a05
Vera Nekhaeva <heliarian@95.108.174.99-red.dhcp.yndx.net>
2014-05-28 16:44:23 +0400

    Исправлена инициализация i-model на DOM

--- common.blocks/i-bem/__dom/i-bem__dom.js
+++ common.blocks/i-bem/__dom/i-bem__dom.js
@@ -2,7 +2,11 @@ $.inheritSelf(BEM.DOM, {}, {
     init: function() {
         var nodes = $('.i-model');
 
-        if (nodes.length) nodes.bem('i-model');
+        if (nodes.length) {
+            nodes.each(function(i, node) {
+                $(node).bem('i-model')
+            });
+        }
 
         this.__base.apply(this, arguments);
     }
